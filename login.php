151604c4c4b379cb4cba675732d7d7cd15fbb971
Guilherme Serpa Batista <serpa419@msn.com>
2019-10-07 21:51:45 -0300

    login img

--- login.php
+++ login.php
@@ -41,7 +41,9 @@ include "db.php";
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+              <div class="col-lg-6 d-none d-lg-block">
+                <img src="img/Uemg.png">
+              </div>
               <div class="col-lg-6">
                 <div class="p-5">
                   <div class="text-center">
